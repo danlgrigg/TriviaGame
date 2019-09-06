710820944962eba9ff190c61955fbe84071aa366
danlgrigg <griggdl@gmail.com>
2019-09-05 19:27:08 -0700

    Created HTML framework with Bootstrap.

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+//create listening event
+$(document).ready
+//create a starting page? start button?  Hide function?
+
+//create object of 10 questions and answers and wrong answers 
+
+//create interval for display clock on key event
+//create variables for 
+//create score counter for correct incorrect; 1 out of 10
+//create random event to pick question from array 
+//set conditions for correct incorrect answers
+//create end page to display correct/incorrect/unanswered? Hide/appear function?
+//create a reset game function
